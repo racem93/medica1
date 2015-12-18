66998a11e3e00d522cf3c95cd8c23efc6690976d
Racem <jedidi.racem@gmail.com>
2015-12-18 14:03:33 +0100

    finalfirstcommit

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
 <?php } ?>
 </div><!--/fluid-row-->
 <?php if (!isset($no_visible_elements) || !$no_visible_elements) { ?>
-
-    /*<!-- Ad, you can remove it -->
+<?php /*
+    <!-- Ad, you can remove it -->
     <div class="row">
         <div class="col-md-9 col-lg-9 col-xs-9  hidden-xs">
             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -27,7 +27,7 @@
         </div>
 
     </div>
-    <!-- Ad ends -->*/
+    <!-- Ad ends --> */ ?>
 
     <hr>
 
@@ -64,38 +64,38 @@
 
 <!-- external javascript -->
 
-<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
 <!-- library for cookie management -->
 <script src="js/jquery.cookie.js"></script>
 <!-- calender plugin -->
-<script src='../bower_components/moment/min/moment.min.js'></script>
-<script src='../bower_components/fullcalendar/dist/fullcalendar.min.js'></script>
+<script src='bower_components/moment/min/moment.min.js'></script>
+<script src='bower_components/fullcalendar/dist/fullcalendar.min.js'></script>
 <!-- data table plugin -->
-<script src='../js/jquery.dataTables.min.js'></script>
+<script src='js/jquery.dataTables.min.js'></script>
 
 <!-- select or dropdown enhancer -->
-<script src="../bower_components/chosen/chosen.jquery.min.js"></script>
+<script src="bower_components/chosen/chosen.jquery.min.js"></script>
 <!-- plugin for gallery image view -->
-<script src="../bower_components/colorbox/jquery.colorbox-min.js"></script>
+<script src="bower_components/colorbox/jquery.colorbox-min.js"></script>
 <!-- notification plugin -->
-<script src="../js/jquery.noty.js"></script>
+<script src="js/jquery.noty.js"></script>
 <!-- library for making tables responsive -->
-<script src="../bower_components/responsive-tables/responsive-tables.js"></script>
+<script src="bower_components/responsive-tables/responsive-tables.js"></script>
 <!-- tour plugin -->
-<script src="../bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js"></script>
+<script src="bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js"></script>
 <!-- star rating plugin -->
-<script src="../js/jquery.raty.min.js"></script>
+<script src="js/jquery.raty.min.js"></script>
 <!-- for iOS style toggle switch -->
-<script src="../js/jquery.iphone.toggle.js"></script>
+<script src="js/jquery.iphone.toggle.js"></script>
 <!-- autogrowing textarea plugin -->
-<script src="../js/jquery.autogrow-textarea.js"></script>
+<script src="js/jquery.autogrow-textarea.js"></script>
 <!-- multiple file upload plugin -->
-<script src="../js/jquery.uploadify-3.1.min.js"></script>
+<script src="js/jquery.uploadify-3.1.min.js"></script>
 <!-- history.js for cross-browser state change on ajax -->
-<script src="../js/jquery.history.js"></script>
+<script src="js/jquery.history.js"></script>
 <!-- application script for Charisma demo -->
-<script src="../js/charisma.js"></script>
+<script src="js/charisma.js"></script>
 
 <?php //Google Analytics code for tracking my demo site, you can remove this.
 if ($_SERVER['HTTP_HOST'] == 'usman.it') {
