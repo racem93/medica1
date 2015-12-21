b94f83df6adc17d47e95ec61f4da6de8e7fbd0c6
Racem <jedidi.racem@gmail.com>
2015-12-21 16:13:21 +0100

    ajout_lit

--- ajout_lit.php
+++ ajout_lit.php
@@ -1,8 +1,13 @@
 <?php if (isset($_POST["lit"])){
     extract($_POST);
+    include_once("MyPDO.class.php");
+    $connect=new MyPDO();
+    $etat_lit=1;
+    $req1 = "INSERT INTO `lit`( `nom`,`ref_lit`, `ref_moteur_p`, `ref_moteur_s`, `ref_telecommande`, `etat_base`, `etat_barriere`, `etat_panneaux`, `etat_moteur`, `etat_variable`, `etat_releve`, `etat_telecommande`, `etat_perroquet`, `description`, `etat_lit`)
+    VALUES ("."'".$nom."'".","."'".$ref_lit."'".","."'".$ref_moteur_p."'".","."'".$ref_moteur_s."'".","."'".$ref_telecommande."'".","."'".$etat_base."'".","."'".$etat_barriere."'".","."'".$etat_panneaux."'".","."'".$etat_moteur."'".","."'".$etat_variable."'".","."'".$etat_releve."'".","."'".$etat_telecommande."'".","."'".$etat_perroquet."'".","."'".$description."'".","."'".$etat_lit."'".")";
+    $oPDOStatement=$connect->query($req1); // Le résultat est un objet de la classe PDOStatement
 
 } ?>
-
 
 
 
@@ -40,11 +45,19 @@
     <div class="control-group">
         <form role="form" action="ajout_lit.php" method="post">
             <div class="form-inline">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-3"><label class="control-label"><h3 style="display:inline;"><b>MODELE DE LIT:</b></h3></label></div>
+                    <div class="col-md-8"> <input type="text" class="form-control" style="width: 500px;" name="nom" ></div>
+                </div>
+            </div>
+            <br>
+            <div class="form-inline">
                 <table class="table table-striped table-bordered responsive" border="">
                     <tr>
                     <td class="col-md-2" ><h4 style="display:inline;"><b>REF. DU LIT:</b></h4></td>
                     <td class="col-md-8" ><h1 style="display:inline;">MM2-L
-                    <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px" name="ref_lit" autofocus required>
+                    <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px;" name="ref_lit" autofocus required>
                     -S</h1></td>
                     </tr>
                     <tr>
@@ -170,6 +183,15 @@
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
                         </select>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="controls" >
+                <div class="row">
+                    <div class="col-md-4">AUTRES ACCESSOIRES/DESCRIPTION</div>
+                    <div class="col-md-6">
+                        <textarea class="form-control " name="description"></textarea>
                     </div>
                 </div>
             </div>
