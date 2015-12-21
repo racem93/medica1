308139eacfbb0731664cff9a47009cf11b71f5a4
Racem <jedidi.racem@gmail.com>
2015-12-21 13:43:49 +0100

    ajout_lit

--- ajout_lit.php
+++ ajout_lit.php
@@ -1,3 +1,11 @@
+<?php if (isset($_POST["lit"])){
+    extract($_POST);
+
+} ?>
+
+
+
+
 <?php require('header.php'); ?>
 <div>
     <ul class="breadcrumb">
@@ -30,31 +38,31 @@
             </div>
 <div class="box-content">
     <div class="control-group">
-        <form role="form">
+        <form role="form" action="ajout_lit.php" method="post">
             <div class="form-inline">
                 <table class="table table-striped table-bordered responsive" border="">
                     <tr>
                     <td class="col-md-2" ><h4 style="display:inline;"><b>REF. DU LIT:</b></h4></td>
                     <td class="col-md-8" ><h1 style="display:inline;">MM2-L
-                    <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px">
+                    <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px" name="ref_lit" autofocus required>
                     -S</h1></td>
                     </tr>
                     <tr>
                         <td class="col-md-2"><b>REF. MOTEUR PRINCIPAL:</b></td>
                         <td class="col-md-8" ><h3 style="display:inline;">MM2-L
-                                <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px">
+                                <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px" name="ref_moteur_p">
                                 -MP</h3></td>
                     </tr>
                     <tr>
                         <td class="col-md-2"><b>REF. MOTEUR R-B:</b></td>
                         <td class="col-md-8" ><h3 style="display:inline;">MM2-L
-                                <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px">
+                                <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px" name="ref_moteur_s">
                                 -MA</h3></td>
                     </tr>
                     <tr>
                         <td class="col-md-2"><b>REF. TELECOMMANDE:</b></td>
                         <td class="col-md-8" ><h3 style="display:inline;">MM2-L
-                                <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px">
+                                <input type="text" class="form-control" id="exampleInputEmail1"  style="width: 50px" name="ref_telecommande">
                                 -MC</h3></td>
                     </tr>
 
@@ -66,7 +74,7 @@
             <div class="row">
                 <div class="col-md-4">ETAT DE LA BASE DU LIT</div>
                 <div class="col-md-8">
-            <select id="selectError" data-rel="chosen" class="col-md-8">
+            <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_base">
                 <option selected value="fonctionne">Fonctionne</option>
                 <option value="casse">Cassé</option>
                 <option value="perdu">Perdu</option>
@@ -79,7 +87,7 @@
                 <div class="row">
                     <div class="col-md-4">ETAT DES BARRIERES</div>
                     <div class="col-md-8">
-                        <select id="selectError" data-rel="chosen" class="col-md-8">
+                        <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_barriere">
                             <option selected value="fonctionne">Fonctionne</option>
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
@@ -92,7 +100,7 @@
                 <div class="row">
                     <div class="col-md-4">ETAT DES PANNEAUX</div>
                     <div class="col-md-8">
-                        <select id="selectError" data-rel="chosen" class="col-md-8">
+                        <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_panneaux">
                             <option selected value="fonctionne">Fonctionne</option>
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
@@ -105,7 +113,7 @@
                 <div class="row">
                     <div class="col-md-4">ETAT MOTEUR CENTRAL</div>
                     <div class="col-md-8">
-                        <select id="selectError" data-rel="chosen" class="col-md-8">
+                        <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_moteur">
                             <option selected value="fonctionne">Fonctionne</option>
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
@@ -118,7 +126,7 @@
                 <div class="row">
                     <div class="col-md-4">ETAT HAUT.VARIABLE</div>
                     <div class="col-md-8">
-                        <select id="selectError" data-rel="chosen" class="col-md-8">
+                        <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_variable">
                             <option selected value="fonctionne">Fonctionne</option>
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
@@ -131,7 +139,7 @@
                 <div class="row">
                     <div class="col-md-4">ETAT RELEVE BUSTE</div>
                     <div class="col-md-8">
-                        <select id="selectError" data-rel="chosen" class="col-md-8">
+                        <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_releve">
                             <option selected value="fonctionne">Fonctionne</option>
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
@@ -144,7 +152,7 @@
                 <div class="row">
                     <div class="col-md-4">ETAT TELECOMMANDE</div>
                     <div class="col-md-8">
-                        <select id="selectError" data-rel="chosen" class="col-md-8">
+                        <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_telecommande">
                             <option selected value="fonctionne">Fonctionne</option>
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
@@ -157,13 +165,18 @@
                 <div class="row">
                     <div class="col-md-4">ETAT DU PERROQUET</div>
                     <div class="col-md-8">
-                        <select id="selectError" data-rel="chosen" class="col-md-8">
+                        <select id="selectError" data-rel="chosen" class="col-md-8" name="etat_perroquet">
                             <option selected value="fonctionne">Fonctionne</option>
                             <option value="casse">Cassé</option>
                             <option value="perdu">Perdu</option>
                         </select>
                     </div>
                 </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-4"></div>
+            <button type="submit" class="btn btn-primary" name="lit" style="width: 200px" >Ajouter</button>
             </div>
             </form>
     </div>
